7757829816a89e06a1d6c88a1e2087b416ea6806
Daniel Walder <walder.daniel@gmx.at>
2019-08-10 16:56:02 +0200

    last exercises

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const auth = require('./middleware/authentication')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(auth.getTokenFrom)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogsRouter.get('/', async (request, response, next) => {
   try{
@@ -15,7 +17,16 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
 
+  const blog = await Blog.findById(request.params.id)
+ 
   try{
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }else if(decodedToken.id !== blog.user.toString()){
+      return response.status(401).json({ error: 'only the creator is allowed to delete this blog' })
+    }
     await Blog.findByIdAndRemove(request.params.id)
     return response.status(204).end()
   } catch(exception){
@@ -41,10 +52,18 @@ blogsRouter.put('/:id', async (request, response, next) => {
   }
 })
   
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) =>{ 
 
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = request.token
+    try {
+      const decodedToken = jwt.verify(token, process.env.SECRET)
+      if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+      }
+  
+
+    const user = await User.findById(decodedToken.id)
     var blog = null
 
     if(body.author === undefined || body.url === undefined){
@@ -56,17 +75,17 @@ blogsRouter.post('/', async (request, response, next) => {
         title: body.title,
         author: body.author,
         likes: 0,
-        user: body.userId
+        user: user._id
       })
     } else{
       blog = new Blog({
         title: body.title,
         author: body.author,
         likes: body.likes,
-        user: body.userId
+        user: user._id
       })
     }
-    try{
+   
       const savedBlog = await blog.save()
       if(user !== null){
         user.blogs = user.blogs.concat(savedBlog._id)
